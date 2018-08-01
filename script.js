fa9f30b47a5bf1c827097bf107178938cd6bde7a
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-08-01 18:23:55 +0900

    変数名整理

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ var dataset = [
     }
 ];
 
-var nodes = d3.select("#xxx")
+var $3nodes = d3.select("#xxx")
     .selectAll("g")
     .data(dataset)
     .enter()
     .append("g")
     .each(function(d){
-        d.bindedElement = this;
+        d.$3bindedElement = this;
     });
 
-var box = nodes.append("rect")
+var $3txtContainer = $3nodes.append("rect")
     .attr("fill", "rgb(0,0,100)");
 
-var txtCntnt = nodes.append("text")
+var $3txtCntnt = $3nodes.append("text")
     .attr("class", "caption")
     .attr("x", 100)
     .attr("y", 200)
@@ -41,65 +41,64 @@ var txtCntnt = nodes.append("text")
         }
     });
 
-box.attr("x", txtCntnt.node().getBBox().x)
-    .attr("y", txtCntnt.node().getBBox().y)
-    .attr("width", txtCntnt.node().getBBox().width + 30)
-    .attr("height", txtCntnt.node().getBBox().height + 30);
+$3txtContainer.attr("x", $3txtCntnt.node().getBBox().x)
+    .attr("y", $3txtCntnt.node().getBBox().y)
+    .attr("width", $3txtCntnt.node().getBBox().width + 30)
+    .attr("height", $3txtCntnt.node().getBBox().height + 30);
 
-nodes.on("dblclick",function(d){dblClicked(d);})
+$3nodes.on("dblclick",function(d){dblClicked(d);})
 
 function dblClicked(d){
 
-    var childNode = d.bindedElement.childNodes;
+    var childNode = d.$3bindedElement.childNodes;
     
     //caption検索ループ
-    var D3captionElem;
+    var $3captionElem;
     for(var i = 0 ; i < childNode.length ; i++){
-        var D3captionElem = d3.select(childNode[i]);
-        if(D3captionElem.classed("caption")){
+        var $3captionElem = d3.select(childNode[i]);
+        if($3captionElem.classed("caption")){
             break;
         }
     }
 
-    //caption取得
-    var cptn = D3captionElem.text();
-
     //フォントの取得
-    var fntFam = D3captionElem.attr("font-family");
+    var fntFam = $3captionElem.attr("font-family");
     if(!fntFam){ //フォントが指定されていない場合
-        fntFam = window.getComputedStyle(D3captionElem.node()).fontFamily; //ブラウザが計算したフォントを取得
+        fntFam = window.getComputedStyle($3captionElem.node()).fontFamily; //ブラウザが計算したフォントを取得
     }
     //フォントサイズの取得
-    var fntSiz = D3captionElem.attr("font-size");
+    var fntSiz = $3captionElem.attr("font-size");
     if(!fntSiz){ //フォントサイズが指定されていない場合
-        fntSiz = window.getComputedStyle(D3captionElem.node()).fontSize; //ブラウザが計算したサイズを取得
+        fntSiz = window.getComputedStyle($3captionElem.node()).fontSize; //ブラウザが計算したサイズを取得
     }
     
-    var D3txtArea = d3.select("body").append("textarea")
+    //textareaの表示
+    var $3txtArea = d3.select("body").append("textarea")
         .style("position", "absolute")
         .style("left", 200 + "px")
         .style("top", 300 + "px")
         .style("font-family", fntFam)
         .style("font-size", fntSiz)
-        .property("value", cptn)
+        .property("value", $3captionElem.text())
         .attr("wrap","off");
+        
+    $3txtArea.node().focus();
 
     //textareaのサイズ自動調整
-    var tmpNode = D3txtArea.node();
-
-    D3txtArea.attr("data-scrollWidthBefore", tmpNode.scrollWidth)
+    var tmpNode = $3txtArea.node();
+    $3txtArea.attr("data-scrollWidthBefore", tmpNode.scrollWidth)
         .attr("data-scrollHeightBefore", tmpNode.scrollHeight)
         .node().oninput = function(){
             
             //サイズ調整
-            if( tmpNode.scrollWidth > D3txtArea.attr('data-scrollWidthBefore')){ //width不足の場合
-                D3txtArea.style("width", (tmpNode.scrollWidth + 15) + "px");
-                D3txtArea.attr('data-scrollWidthBefore', tmpNode.scrollWidth);
+            if( tmpNode.scrollWidth > $3txtArea.attr('data-scrollWidthBefore')){ //width不足の場合
+                $3txtArea.style("width", (tmpNode.scrollWidth + 15) + "px");
+                $3txtArea.attr('data-scrollWidthBefore', tmpNode.scrollWidth);
             }
 
-            if( tmpNode.scrollHeight > D3txtArea.attr('data-scrollHeightBefore')){ //height不足の場合
-                D3txtArea.style("height", (tmpNode.scrollHeight + 15) + "px");
-                D3txtArea.attr('data-scrollHeightBefore', tmpNode.scrollHeight);
+            if( tmpNode.scrollHeight > $3txtArea.attr('data-scrollHeightBefore')){ //height不足の場合
+                $3txtArea.style("height", (tmpNode.scrollHeight + 15) + "px");
+                $3txtArea.attr('data-scrollHeightBefore', tmpNode.scrollHeight);
             }
 
         };
