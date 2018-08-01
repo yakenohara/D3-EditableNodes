846fe48f0c202cf17033f59504190a13e368052a
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-08-01 18:16:43 +0900

    テキストボックスサイズの自動変更

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var dataset = ["untitled"];
+var dataset = ["untitled yg あいう"];
 
 var node = d3.select("#xxx")
     .selectAll("g")
@@ -7,32 +7,43 @@ var node = d3.select("#xxx")
     .append("g");
 
 var box = node.append("rect")
-    .attr("x", 100)
-    .attr("y", 200)
     .attr("fill", "rgb(0,0,100)");
 
 var txtCntnt = node.append("text")
     .attr("x", 100)
     .attr("y", 200)
-    .attr("dominant-baseline", "text-before-edge")
+    // .attr("font-family", "Impact,sans-serif")
     .attr("fill", "White")
     .text(function(d, i){return d;});
 
-box.attr("width", txtCntnt.node().getBBox().width)
+box.attr("x", txtCntnt.node().getBBox().x)
+    .attr("y", txtCntnt.node().getBBox().y)
+    .attr("width", txtCntnt.node().getBBox().width)
     .attr("height", txtCntnt.node().getBBox().height);
 
 node.on("dblclick", cli);
 
-var cnt = 0;
-
 function cli(){
-    d3.select("body").append("textarea")
+    var tmp = d3.select("body").append("textarea")
         .style("position", "absolute")
         .style("left", 200 + "px")
         .style("top", 300 + "px")
-        .attr("wrap","off")
+        .attr("wrap","off");
+
+    tmp.attr("data-scrollWidthBefore", tmp.node().scrollWidth)
+        .attr("data-scrollHeightBefore", tmp.node().scrollHeight)
         .node().oninput = function(){
-            console.log(`cnt:${cnt}`);
-            cnt++;
+            
+            //サイズ調整
+            if( this.scrollWidth > this.getAttribute('data-scrollWidthBefore')){ //width不足の場合
+                this.style.width = (this.scrollWidth + 15) + "px";
+                this.setAttribute('data-scrollWidthBefore', this.scrollWidth);
+            }
+
+            if( this.scrollHeight > this.getAttribute('data-scrollHeightBefore')){ //height不足の場合
+                this.style.height = (this.scrollHeight + 15) + "px";
+                this.setAttribute('data-scrollHeightBefore', this.scrollHeight);
+            }
+
         };
 }
