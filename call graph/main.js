a098630cfbb693fbbcf485869402d0191af9633f
yakenohara <38835246+yakenohara@users.noreply.github.com>
2019-06-16 22:46:42 +0900

    scope tree に object list を追加

--- call graph/main.js
+++ call graph/main.js
@@ -61,27 +61,14 @@ if(bl_enable_output_AST_obj_as_JSON_file){ //パース時に生成された AST 
     );
 }
 
-var arr_depth_stack = [];
-
-var arr_function_declarations = [];
-var arr_call_expressions = [];
+console.log("# Adding scope info");
 
 (function () {
+    var arr_depth_stack = [];
 
-    /**
-     * ASTを深さ優先探索で巡回する。
-     * @param {AstNode} root ASTのルートノード。
-     * @param {EstraverseVisitor} visitor 巡回オブジェクト（Visitorパターン）。
-     */
     mod_estraverse.traverse(
         obj_AST_of_js_code,
         {
-            /**
-             * ノードに訪れたときに実行される。thisにestraverse.Controllerのインスタンスにアクセスできる。
-             * @param {AstNode} obj_current_node 訪問したノード。
-             * @param {AstNode} obj_parent_node 訪問したノードの親ノード。
-             * @this {estraverse.Controller}
-             */
             enter: function(obj_current_node, obj_parent_node) {
 
                 //<現在の node の位置を arr_depth_stack[] に保存する>----------------------------------
@@ -114,175 +101,203 @@ var arr_call_expressions = [];
                 }());
                 
                 //---------------------------------</現在の node の位置を arr_depth_stack[] に保存する>
-                
-                if(obj_current_node.type == "FunctionDeclaration"){
 
-                    //引数表示用文字列の作成
-                    var str_arg = ""
-                    var num_i = 0;
-                    if(num_i < obj_current_node.params.length){
-                        str_arg = obj_current_node.params[num_i].name;
-                    }
-                    for(num_i = 1 ; num_i < obj_current_node.params.length ; num_i++){
-                        str_arg += ", " + obj_current_node.params[num_i].name;
-                    }
-                    console.log(obj_current_node.id.name + "(" + str_arg + ")");
-
-                    // var num_idx_of_depth_stack;
-                    // for(num_idx_of_depth_stack = (arr_depth_stack.length - 2) ; num_idx_of_depth_stack >= 0 ; num_idx_of_depth_stack--){
-                    //     var str_type = arr_depth_stack[num_idx_of_depth_stack].type;
-                    //     if(str_type == "Program" || str_type == "FunctionDeclaration"){
-                    //         break;
-                    //     }
-                    // }
-
-                    // var arr_tmp_scope = [];
-                    // var num_i = 0;
-                    // for(num_i = 0 ; num_i <= num_idx_of_depth_stack ; num_i ++){
-                    //     arr_tmp_scope.push(arr_depth_stack[num_i]);
-                    // }
-
-                    arr_function_declarations.push({
-                        obj_AST_node:obj_current_node,
-                        arr_scope_stack:fnc_make_scope_stack(obj_current_node)
-                    });
-                }
-
-                if(obj_current_node.type == "CallExpression"){
-
-                    switch(obj_current_node.callee.type){
-                        case 'Identifier':
-                        {
-                            //<note>
-                            //
-                            // "type": "CallExpression",
-                            // "callee": {
-                            //     "type": "Identifier",
-                            //     "name": <関数名>
-                            // },
-                            // "arguments": [
-                            //     {
-                            //         "type": "Literal",
-                            //         "value": 2,
-                            //         "raw": "2"
-                            //     },
-                            //     {
-                            //         "type": "Literal",
-                            //         "value": 3,
-                            //         "raw": "3"
-                            //     }
-                            // ]
-                            //
-
-                            arr_call_expressions.push({
-                                obj_AST_node:obj_current_node,
-                                arr_scope_stack_of_caller:fnc_make_scope_stack(obj_current_node)
-                            });
-
-                            break;
+                switch(obj_current_node.type){
+                    case 'Program':
+                    case 'VariableDeclarator':
+                    case 'FunctionExpression':
+                    case 'FunctionDeclaration':
+                    case 'AssignmentExpression':
+                    case 'CallExpression':
+                    case 'ReturnStatement':
+                    {
+                        switch(obj_current_node.type){
+                            case 'Program':
+                            case 'FunctionExpression':
+                            case 'FunctionDeclaration':
+                            {
+                                if((typeof obj_current_node.TraversedObj_BelongingObjects) == 'undefined'){
+                                    obj_current_node.TraversedObj_BelongingObjects = [];
+                                    obj_current_node.TraversedObj_NestedFunctions = [];
+                                    obj_current_node.TraversedObj_Closurings = [];
+                                }
+                                break;
+                            }
+                            default:
+                            {
+                                //nothing to do
+                                break;
+                            }
                         }
-                        case 'MemberExpression':
-                        {
-                            //<note>
-                            //
-                            // "type": "CallExpression",
-                            // "callee": {
-                            //     "type": "MemberExpression",
-                            //     "computed": false,
-                            //     "object": {
-                            //         "type": "Identifier",
-                            //         "name": "console"
-                            //     },
-                            //     "property": {
-                            //         "type": "Identifier",
-                            //         "name": "log"
-                            //     }
-                            // },
-                            // "arguments": [
-                            //     {
-                            //         "type": "Identifier",
-                            //         "name": <変数名>
-                            //     }
-                            // ]
-                            console.error("unknown callee type found");
-                            break;
+
+                        var obj_parent_function = null;
+                        for(var i =arr_depth_stack.length-2 ; i >= 0 ; i--){
+                            switch(arr_depth_stack[i].type){
+                                case 'Program':
+                                case 'FunctionExpression':
+                                case 'FunctionDeclaration':
+                                {
+                                    obj_parent_function = arr_depth_stack[i];
+                                    switch(obj_current_node.type){
+                                        case 'VariableDeclarator':
+                                        {
+                                            arr_depth_stack[i].TraversedObj_BelongingObjects.push(obj_current_node);
+                                            arr_depth_stack[i].TraversedObj_Closurings.push(obj_current_node);
+                                            break;
+                                        }
+                                        case 'FunctionExpression':
+                                        {
+                                            arr_depth_stack[i].TraversedObj_NestedFunctions.push(obj_current_node);
+                                            break;
+                                        }
+                                        case 'FunctionDeclaration':
+                                        {
+                                            arr_depth_stack[i].TraversedObj_BelongingObjects.push(obj_current_node);
+                                            arr_depth_stack[i].TraversedObj_NestedFunctions.push(obj_current_node);
+                                            break;
+                                        }
+                                        case 'AssignmentExpression':
+                                        case 'CallExpression':
+                                        case 'ReturnStatement':
+                                        {
+                                            arr_depth_stack[i].TraversedObj_Closurings.push(obj_current_node);
+                                            break;
+                                        }
+                                        default:
+                                        {
+                                            //nothing to do
+                                            break;
+                                        }
+                                    }
+
+                                    i = 0; //break `for` statement
+                                    break;
+                                }
+                                default:
+                                {
+                                    //nothing to do
+                                    break;
+                                }
+                            }
                         }
-                        default:
-                        {
-                            console.error("unknown callee type found");
-                            break;
-                        }    
+
+                        switch(obj_current_node.type){
+                            case 'Program':
+                            case 'FunctionExpression':
+                            case 'FunctionDeclaration':
+                            {
+                                obj_current_node.TraversedObj_ParentFunction = obj_parent_function;
+                                break;
+                            }
+                            default:
+                            {
+                                //nothing to do
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                    default:
+                    {
+                        //nothing to do
+                        break;
                     }
                 }
             }
         }
     );
-
-    function fnc_make_scope_stack(obj_current_node){
-
-        var arr_scope_stack = [];
-
-        for(var num_idx_of_depth_stack = 0 ; num_idx_of_depth_stack < (arr_depth_stack.length-1) ; num_idx_of_depth_stack++){
-            var str_type = arr_depth_stack[num_idx_of_depth_stack].type;
-            if(str_type == "Program" || str_type == "FunctionDeclaration"){
-                arr_scope_stack.push(arr_depth_stack[num_idx_of_depth_stack]);
-            }
-        }
-
-        return arr_scope_stack;
-    }
-
 }());
 
-console.log("\n# FunctionDeclarations\n");
+console.log("\n# Closurings");
 
-arr_function_declarations.forEach(function(obj_function_declaration){
+(function () {
 
-    //引数表示用文字列の作成
-    var str_arg = ""
-    var num_i = 0;
-    if(num_i < obj_function_declaration.obj_AST_node.params.length){
-        str_arg = obj_function_declaration.obj_AST_node.params[num_i].name;
-    }
-    for(num_i = 1 ; num_i < obj_function_declaration.obj_AST_node.params.length ; num_i++){
-        str_arg += ", " + obj_function_declaration.obj_AST_node.params[num_i].name;
-    }
-    console.log(obj_function_declaration.obj_AST_node.id.name + "(" + str_arg + ")");
-    
-    
-    var str = "";
-    var i = 0;
-    if(i < obj_function_declaration.arr_scope_stack.length){
-        str += obj_function_declaration.arr_scope_stack[i].type;
-    }
-    for(i = 1 ; i < obj_function_declaration.arr_scope_stack.length ; i ++){
-        str += "/" + obj_function_declaration.arr_scope_stack[i].type;
-    }
-    console.log("scope:" + str);
-});
+    var arr_depth_stack = [];
 
-console.log("\n# Function Caller, Callees\n");
+    mod_estraverse.traverse(
+        obj_AST_of_js_code,
+        {
+            enter: function(obj_current_node, obj_parent_node) {
 
-arr_call_expressions.forEach(function(obj_call_expression){
+                //<現在の node の位置を arr_depth_stack[] に保存する>----------------------------------
 
-    var obj_function_declaration_of_caller = obj_call_expression.arr_scope_stack_of_caller[obj_call_expression.arr_scope_stack_of_caller.length-1];
+                if(typeof obj_parent_node == 'object'){
+                    var num_idx_of_depth_stack;
+                    for(num_idx_of_depth_stack = (arr_depth_stack.length - 1) ; num_idx_of_depth_stack >= 0 ; num_idx_of_depth_stack--){
+                        if(arr_depth_stack[num_idx_of_depth_stack] == obj_parent_node){
+                            break;
+                        }
+                    }
 
-    var str_arg = ""
-    if(obj_function_declaration_of_caller.type == 'FunctionDeclaration'){
+                    if(num_idx_of_depth_stack < (arr_depth_stack.length - 1)){
+                        arr_depth_stack.splice(num_idx_of_depth_stack+1, arr_depth_stack.length-(num_idx_of_depth_stack+1));
+                    }
+                }
+                arr_depth_stack.push(obj_current_node);
 
-        var num_i = 0;
-        if(num_i < obj_function_declaration_of_caller.params.length){
-            str_arg = obj_function_declaration_of_caller.params[num_i].name;
+                //デバッグ用表示 (for arr_depth_stack[])
+                (function () {
+                    var str = "";
+                    var i = 0;
+                    if(i < arr_depth_stack.length){
+                        str += arr_depth_stack[i].type;
+                    }
+                    for(i = 1 ; i < arr_depth_stack.length ; i ++){
+                        str += "/" + arr_depth_stack[i].type;
+                    }
+                    console.log(str);
+                }());
+                
+                //---------------------------------</現在の node の位置を arr_depth_stack[] に保存する>
+
+                switch(obj_current_node.type){
+                    case 'Program':
+                    case 'FunctionExpression':
+                    case 'FunctionDeclaration':
+                    {
+                        for(var i = 0 ; i < obj_current_node.TraversedObj_Closurings.length ; i++){
+                            var obj_closuring = obj_current_node.TraversedObj_Closurings[i];
+                            console.log("  TraversedObj_Closurings[" + String(i) + "].type: " + obj_closuring.type);
+                            
+                            switch(obj_closuring.type){
+                                case 'VariableDeclarator':
+                                {
+                                    if(typeof obj_closuring.id.TraversedObj_Closures == 'undefined'){
+                                        obj_closuring.id.TraversedObj_Closures = [];
+                                    }
+                                    obj_closuring.id.TraversedObj_Closures.push(obj_closuring.init);
+                                    break;
+                                }
+                                case 'AssignmentExpression':
+                                {
+                                    switch(obj_closuring.left.type){
+                                        case 'Identifier':
+                                        {
+                                            for(var j = 0 ; j < obj_current_node.TraversedObj_BelongingObjects.length ; j++){
+                                            }
+                                            break;
+                                        }
+                                        default:
+                                        {
+                                            //todo 別のパターンがないかどうか
+                                        }
+                                    }
+                                    break;
+                                }
+                                case 'CallExpression':
+                                case 'ReturnStatement':
+                            }
+                        }
+                        break;
+                    }
+                    default:
+                    {
+                        //nothing todo
+                        break;
+                    }
+                }
+            }
         }
-        for(num_i = 1 ; num_i < obj_function_declaration_of_caller.params.length ; num_i++){
-            str_arg += ", " + obj_function_declaration_of_caller.params[num_i].name;
-        }
-        str_arg = obj_function_declaration_of_caller.id.name + "(" + str_arg + ")";
-
-    }else{
-        str_arg = "Program";
-    }
-
-    console.log("Caller:" + str_arg);
-
-});
+    );
+}());
